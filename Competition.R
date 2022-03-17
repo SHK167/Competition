9872d914bc20469f851b6c03ede03c4462cf2075
Shri Krishna Kumar <shk167@pitt.edu>
2022-03-17 11:55:57 -0400

    Create Competition.R
    This is basic step of loading the data to GitHub.

---
+++ Competition.R
@@ -0,0 +1,12 @@
+# COMPETITION
+# read data 
+
+data<- read.csv("https://raw.githubusercontent.com/PittDataScience/CourseContent/master/Assignments/competition/competition-data.csv?token=GHSAT0AAAAAABSJ23EEZ74BPHO5546DNXMQYRSVYNQ")
+test_data <- read.csv("https://raw.githubusercontent.com/PittDataScience/CourseContent/master/Assignments/competition/competition-test-x-values.csv?token=GHSAT0AAAAAABSJ23EFB5P55YBCQWUQQWKGYRSVZKA")
+
+
+
+
+
+
+
