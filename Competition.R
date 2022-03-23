19dd2b471d7da0f3a5ca0672c3c8e4e81a5b8d7a
calvintien1998 <calvintien2019@gmail.com>
2022-03-23 16:26:23 -0400

    liner regression
    linear regression fit.

--- Competition.R
+++ Competition.R
@@ -329,6 +329,26 @@ min(ThecartTune$results$RMSE)
 ThecartTune$bestTune
 
 
+folds <- createFolds(pca_train_outcome, k = 2, returnTrain = TRUE)
+ctrl <- trainControl(method = "cv", index = folds)
+
+# LINEAR REGRESSION
+
+
+set.seed(100)
+lmTune<- train(x= pca_train_predictors, y= pca_train_outcome,
+               method = "lm",
+               trControl = ctrl)
+lmTune
+print(lmTune$results)
+### Evaluating the training error for the Linear regression model
+#### RMSE
+
+trainfit_LR <- RMSE( predict(lmTune, pca_train_predictors), pca_train_outcome)
+trainfit_LR
+
+
+
 
 
 
